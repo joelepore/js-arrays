dfd29cf90c4616b97238848c031f6a44768ee4ba
joelepore <giuseppejoelepore@gmail.com>
2024-11-15 14:31:20 +0100

    feat: complete exercise 7

--- 01-arrays/script.js
+++ 01-arrays/script.js
@@ -29,7 +29,7 @@ teachers.unshift('Sarah');
 
 // 7. Verifica se 'Fabio' è presente nell'array teachers
 // e salva il risultato nella variabile isFabioPresent
-const isFabioPresent = null;
+const isFabioPresent = teachers.includes('Fabio');
 
 // 8. Trova l'indice dell'insegnante 'Lewis' nell'array teachers
 // e salvalo nella variabile lewisIndex
